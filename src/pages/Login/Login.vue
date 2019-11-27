47c644604e32ded10a10c8306323ce0b3e7725df
kk <705555262@qq.com>
2019-11-27 21:17:01 +0800

    完成手机号登录界面逻辑

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,16 +4,16 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a href="javascript:;" :class="{on: loginWay}" @click="loginWay=true">短信登录</a>
+          <a href="javascript:;" :class="{on: !loginWay}" @click="loginWay=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on: loginWay}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!rightPhone" class="get_verification" @click.prevent="getCode" :class="{right_phone: rightPhone}">{{computeTime>0? `已发送(${computeTime})s` : '获取验证码'}}</button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -23,7 +23,7 @@
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on: !loginWay}">
             <section>
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
@@ -53,7 +53,36 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginWay: true, // true短信登录,
+      computeTime: 0,
+      phone: ''
+    }
+  },
+  computed: {
+    rightPhone () {
+      return /^1\d{10}$/.test(this.phone)
+    }
+  },
+  methods: {
+    getCode () {
+      if (!this.computeTime) {
+        // 启动倒计时
+        this.computeTime = 30
+        const intervalID = setInterval(() => {
+          this.computeTime -= 1
+          if (this.computeTime <= 0) {
+            // 停止
+            clearInterval(intervalID)
+          }
+        }, 1000)
+        // 发送ajax
+      }
+    }
+  }
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -117,6 +146,8 @@ export default {}
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right_phone
+                  color black
             .login_verification
               position relative
               margin-top 16px
